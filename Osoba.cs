c1c0070bc6ffdebc0ea01629705647d931104f2c
grinrix <146460587+grinrix@users.noreply.github.com>
2025-03-07 20:35:09 +0100

    dodano klase osoba oraz wykładowca

---
+++ Osoba.cs
@@ -0,0 +1,18 @@
+
+namespace TwojProjekt
+{
+    public class Osoba
+    {
+        private string imie_nazwisko;
+
+        public Osoba(string imieNazwisko)
+        {
+            imie_nazwisko = imieNazwisko;
+        }
+
+        public string GetImieNazwisko()
+        {
+            return imie_nazwisko;
+        }
+    }
+}
---
+++ Wykladowca.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+namespace KlasyWykl
+{
+    public class Wykladowca : Osoba
+    {
+        private string wydzial;
+        private List<string> przedmioty;
+        private string tytul;
+
+        public Wykladowca(string imieNazwisko, string wydzial, List<string> przedmioty, string tytul) 
+            : base(imieNazwisko)
+        {
+            this.wydzial = wydzial;
+            this.przedmioty = przedmioty;
+            this.tytul = tytul;
+        }
+
+        public string GetWydzial()
+        {
+            return wydzial;
+        }
+
+        public List<string> GetPrzedmioty()
+        {
+            return przedmioty;
+        }
+
+        public string GetTytul()
+        {
+            return tytul;
+        }
+
+        public void PrzedstawSie()
+        {
+            Console.WriteLine($"{tytul} {GetImieNazwisko()}");
+            Console.WriteLine($"Wydział: {wydzial}");
+            Console.WriteLine("Prowadzone przedmioty:");
+
+            foreach (var przedmiot in przedmioty)
+            {
+                Console.WriteLine($"- {przedmiot}");
+            }
+        }
+    }
+}
