1d9f89b035b148aabbfcd692c7f6e1069f2ceb21
Faanatix <fantixeracc@gmail.com>
2025-03-23 17:20:44 +0100

    Add files via upload

---
+++ Pytanie.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Pytanie
+{
+    private string tresc;
+    public int Id { get; set; }
+    public string Kierunek { get; set; }
+    public string Wykladowca { get; set; }
+
+    public Pytanie(int id, string tresc, string kierunek, string wykladowca)
+    {
+        this.Id = id;
+        this.tresc = tresc;
+        this.Kierunek = kierunek;
+        this.Wykladowca = wykladowca;
+    }
+
+    public string GetTresc()
+    {
+        return tresc;
+    }
+
+    public void SetTresc(string nowaTresc)
+    {
+        tresc = nowaTresc;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Treść: {tresc}, Kierunek: {Kierunek}, Wykładowca: {Wykladowca}";
+    }
+}
