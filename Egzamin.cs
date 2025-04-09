66856ae38c4333c16537fda2e1eccea6d0bf2bbb
Faanatix <fantixeracc@gmail.com>
2025-04-09 22:34:04 +0200

    Add files via upload

---
+++ Egzamin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace egzamin_dyplomowy
+{
+    public class Egzamin
+    {
+        private Student student;
+        private Wykladowca promotor;
+        private Wykladowca recenzent;
+        private Wykladowca prodziekan;
+        private List<Pytanie> pytania;
+
+        public Egzamin(Student student, Wykladowca promotor, Wykladowca recenzent, Wykladowca prodziekan, List<Pytanie> pytania)
+        {
+            this.student = student;
+            this.promotor = promotor;
+            this.recenzent = recenzent;
+            this.prodziekan = prodziekan;
+            this.pytania = pytania;
+        }
+
+        public string GetSala()
+        {
+            return "Jakaś sala";
+        }
+        
+    }
+}
