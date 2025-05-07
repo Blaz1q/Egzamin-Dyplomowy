2781e1026adb63291abdda50bf84097987941af7
grinrix <146460587+grinrix@users.noreply.github.com>
2025-05-07 22:24:27 +0200

    Update logowanie.Designer.cs

--- logowanie.Designer.cs
+++ logowanie.Designer.cs
@@ -40,18 +40,19 @@ namespace egzamin_dyplomowy
             roundedTextBox1.BorderFocusColor = Color.Blue;
             roundedTextBox1.BorderRadius = 7;
             roundedTextBox1.BorderSize = 2;
-            roundedTextBox1.Font = new Font("Instrument Sans", 33.7499962F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            roundedTextBox1.Font = new Font("Instrument Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
             roundedTextBox1.ForeColor = Color.Black;
             roundedTextBox1.InvalidBorderColor = Color.Red;
             roundedTextBox1.InvalidBorderFocusColor = Color.Orange;
-            roundedTextBox1.Location = new Point(52, 156);
+            roundedTextBox1.Location = new Point(59, 208);
+            roundedTextBox1.Margin = new Padding(3, 4, 3, 4);
             roundedTextBox1.Multiline = false;
             roundedTextBox1.Name = "roundedTextBox1";
-            roundedTextBox1.Padding = new Padding(7);
+            roundedTextBox1.Padding = new Padding(20);
             roundedTextBox1.PasswordChar = false;
             roundedTextBox1.PlaceholderColor = Color.DarkGray;
             roundedTextBox1.PlaceholderText = "Twój email";
-            roundedTextBox1.Size = new Size(409, 75);
+            roundedTextBox1.Size = new Size(467, 79);
             roundedTextBox1.TabIndex = 0;
             roundedTextBox1.Texts = "";
             roundedTextBox1.UnderlinedStyle = false;
@@ -63,18 +64,19 @@ namespace egzamin_dyplomowy
             roundedTextBox2.BorderFocusColor = Color.Blue;
             roundedTextBox2.BorderRadius = 7;
             roundedTextBox2.BorderSize = 2;
-            roundedTextBox2.Font = new Font("Instrument Sans", 33.7499962F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            roundedTextBox2.Font = new Font("Instrument Sans", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 238);
             roundedTextBox2.ForeColor = Color.Black;
             roundedTextBox2.InvalidBorderColor = Color.Red;
             roundedTextBox2.InvalidBorderFocusColor = Color.Orange;
-            roundedTextBox2.Location = new Point(52, 290);
+            roundedTextBox2.Location = new Point(59, 387);
+            roundedTextBox2.Margin = new Padding(3, 4, 3, 4);
             roundedTextBox2.Multiline = false;
             roundedTextBox2.Name = "roundedTextBox2";
-            roundedTextBox2.Padding = new Padding(7);
+            roundedTextBox2.Padding = new Padding(20);
             roundedTextBox2.PasswordChar = true;
             roundedTextBox2.PlaceholderColor = Color.DarkGray;
             roundedTextBox2.PlaceholderText = "Twoje hasło";
-            roundedTextBox2.Size = new Size(409, 75);
+            roundedTextBox2.Size = new Size(467, 69);
             roundedTextBox2.TabIndex = 1;
             roundedTextBox2.Texts = "";
             roundedTextBox2.UnderlinedStyle = false;
@@ -90,10 +92,9 @@ namespace egzamin_dyplomowy
             roundedButton1.Font = new Font("Instrument Sans", 23.9999962F, FontStyle.Regular, GraphicsUnit.Point, 238);
             roundedButton1.ForeColor = Color.White;
             roundedButton1.HoverColor = Color.Transparent;
-            roundedButton1.Location = new Point(52, 403);
-            roundedButton1.Margin = new Padding(3, 2, 3, 2);
+            roundedButton1.Location = new Point(59, 537);
             roundedButton1.Name = "roundedButton1";
-            roundedButton1.Size = new Size(409, 70);
+            roundedButton1.Size = new Size(467, 93);
             roundedButton1.TabIndex = 10;
             roundedButton1.Text = "Zaloguj się";
             roundedButton1.TextColor = Color.White;
@@ -106,9 +107,9 @@ namespace egzamin_dyplomowy
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Instrument Sans", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label1.Location = new Point(52, 121);
+            label1.Location = new Point(59, 161);
             label1.Name = "label1";
-            label1.Size = new Size(77, 32);
+            label1.Size = new Size(96, 40);
             label1.TabIndex = 2;
             label1.Text = "Email";
             // 
@@ -117,9 +118,9 @@ namespace egzamin_dyplomowy
             label2.AutoSize = true;
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Instrument Sans", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label2.Location = new Point(52, 255);
+            label2.Location = new Point(59, 340);
             label2.Name = "label2";
-            label2.Size = new Size(78, 32);
+            label2.Size = new Size(98, 40);
             label2.TabIndex = 3;
             label2.Text = "Hasło";
             // 
@@ -128,9 +129,9 @@ namespace egzamin_dyplomowy
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Instrument Sans", 35.9999962F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            label3.Location = new Point(127, 30);
+            label3.Location = new Point(145, 40);
             label3.Name = "label3";
-            label3.Size = new Size(270, 64);
+            label3.Size = new Size(337, 80);
             label3.TabIndex = 6;
             label3.Text = "Zaloguj się";
             // 
@@ -141,9 +142,9 @@ namespace egzamin_dyplomowy
             linkLabel1.Font = new Font("Instrument Sans Medium", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 238);
             linkLabel1.LinkBehavior = LinkBehavior.AlwaysUnderline;
             linkLabel1.LinkColor = Color.Black;
-            linkLabel1.Location = new Point(52, 505);
+            linkLabel1.Location = new Point(59, 673);
             linkLabel1.Name = "linkLabel1";
-            linkLabel1.Size = new Size(237, 32);
+            linkLabel1.Size = new Size(289, 40);
             linkLabel1.TabIndex = 7;
             linkLabel1.TabStop = true;
             linkLabel1.Text = "Zapomniałeś hasła?";
@@ -157,9 +158,9 @@ namespace egzamin_dyplomowy
             linkLabel2.Font = new Font("Instrument Sans Medium", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 238);
             linkLabel2.LinkBehavior = LinkBehavior.AlwaysUnderline;
             linkLabel2.LinkColor = Color.Black;
-            linkLabel2.Location = new Point(48, 554);
+            linkLabel2.Location = new Point(55, 739);
             linkLabel2.Name = "linkLabel2";
-            linkLabel2.Size = new Size(171, 32);
+            linkLabel2.Size = new Size(208, 40);
             linkLabel2.TabIndex = 7;
             linkLabel2.TabStop = true;
             linkLabel2.Text = "Zarejestruj się";
@@ -173,9 +174,9 @@ namespace egzamin_dyplomowy
             linkLabel3.Font = new Font("Instrument Sans Medium", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point, 238);
             linkLabel3.LinkBehavior = LinkBehavior.AlwaysUnderline;
             linkLabel3.LinkColor = Color.Black;
-            linkLabel3.Location = new Point(48, 601);
+            linkLabel3.Location = new Point(55, 801);
             linkLabel3.Name = "linkLabel3";
-            linkLabel3.Size = new Size(175, 32);
+            linkLabel3.Size = new Size(218, 40);
             linkLabel3.TabIndex = 7;
             linkLabel3.TabStop = true;
             linkLabel3.Text = "Aktywuj konto";
@@ -185,24 +186,25 @@ namespace egzamin_dyplomowy
             // pictureBox1
             // 
             pictureBox1.BackColor = Color.White;
-            pictureBox1.Image = Properties.Resources.Logowanie;
-            pictureBox1.Location = new Point(208, -252);
+            pictureBox1.Location = new Point(238, -336);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1558, 1330);
+            pictureBox1.Size = new Size(1781, 1773);
             pictureBox1.TabIndex = 11;
             pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
             pictureBox2.Location = new Point(0, 0);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(520, 820);
+            pictureBox2.Size = new Size(594, 1093);
             pictureBox2.TabIndex = 12;
             pictureBox2.TabStop = false;
             // 
             // logowanie
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             Controls.Add(linkLabel3);
@@ -217,8 +219,9 @@ namespace egzamin_dyplomowy
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             ForeColor = Color.Black;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "logowanie";
-            Size = new Size(1152, 820);
+            Size = new Size(1317, 1093);
             Load += logowanie_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
