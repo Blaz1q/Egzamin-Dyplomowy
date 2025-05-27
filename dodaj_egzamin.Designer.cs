6dd4e99e9feb424085ead48d04f1667336a5ea0e
Oliwia <oliwiakucharska1@o2.pl>
2025-05-27 22:00:33 +0200

    napisałam coś tam
    jest zrobione dziala ale pozmieniam wygląd skibidi,
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    obrona antykrzyśkowa

---
+++ dodaj_egzamin.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace egzamin_dyplomowy
+{
+    partial class dodaj_egzamin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            roundedButton1 = new RoundedButton();
+            roundedButton2 = new RoundedButton();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Elon Musk", "dawid Czech" });
+            comboBox1.Location = new Point(76, 391);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(409, 28);
+            comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label1.Location = new Point(76, 189);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 23);
+            label1.TabIndex = 7;
+            label1.Text = "Recenzent";
+            label1.Click += label1_Click;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(76, 120);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(409, 28);
+            comboBox2.TabIndex = 8;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label2.Location = new Point(76, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 23);
+            label2.TabIndex = 9;
+            label2.Text = "Promotor";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label3.Location = new Point(76, 279);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 23);
+            label3.TabIndex = 10;
+            label3.Text = "Prodziekan";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label4.Location = new Point(76, 368);
+            label4.Name = "label4";
+            label4.Size = new Size(74, 23);
+            label4.TabIndex = 11;
+            label4.Text = "Student";
+            label4.Click += label4_Click;
+            // 
+            // comboBox3
+            // 
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(76, 212);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(409, 28);
+            comboBox3.TabIndex = 12;
+            comboBox3.SelectedIndexChanged += comboBox3_SelectedIndexChanged;
+            // 
+            // comboBox4
+            // 
+            comboBox4.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(76, 302);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(409, 28);
+            comboBox4.TabIndex = 13;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label5.Location = new Point(76, 154);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Pytanie:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label6.Location = new Point(76, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(65, 20);
+            label6.TabIndex = 16;
+            label6.Text = "Pytanie:";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label7.Location = new Point(76, 336);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 20);
+            label7.TabIndex = 17;
+            label7.Text = "Pytanie:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            label8.Location = new Point(76, 36);
+            label8.Name = "label8";
+            label8.Size = new Size(409, 41);
+            label8.TabIndex = 19;
+            label8.Text = "Wybierz komisje i studenta:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(147, 151);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(338, 27);
+            textBox1.TabIndex = 20;
+            textBox1.TextChanged += textBox1_TextChanged_1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(147, 244);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(338, 27);
+            textBox2.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(147, 333);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(338, 27);
+            textBox3.TabIndex = 22;
+            // 
+            // roundedButton1
+            // 
+            roundedButton1.BackColor = Color.FromArgb(64, 64, 64);
+            roundedButton1.BackgroundColor = Color.FromArgb(64, 64, 64);
+            roundedButton1.BorderColor = Color.Black;
+            roundedButton1.BorderRadius = 15;
+            roundedButton1.BorderSize = 0;
+            roundedButton1.FlatAppearance.BorderSize = 0;
+            roundedButton1.FlatStyle = FlatStyle.Flat;
+            roundedButton1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            roundedButton1.ForeColor = Color.White;
+            roundedButton1.HoverColor = Color.Transparent;
+            roundedButton1.Location = new Point(169, 449);
+            roundedButton1.Name = "roundedButton1";
+            roundedButton1.Size = new Size(216, 53);
+            roundedButton1.TabIndex = 23;
+            roundedButton1.Text = "Losuj Pytania";
+            roundedButton1.TextColor = Color.White;
+            roundedButton1.TooltipText = null;
+            roundedButton1.UseVisualStyleBackColor = false;
+            roundedButton1.Click += roundedButton1_Click;
+            // 
+            // roundedButton2
+            // 
+            roundedButton2.BackColor = Color.FromArgb(64, 64, 64);
+            roundedButton2.BackgroundColor = Color.FromArgb(64, 64, 64);
+            roundedButton2.BorderColor = Color.PaleVioletRed;
+            roundedButton2.BorderRadius = 15;
+            roundedButton2.BorderSize = 0;
+            roundedButton2.FlatAppearance.BorderSize = 0;
+            roundedButton2.FlatStyle = FlatStyle.Flat;
+            roundedButton2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            roundedButton2.ForeColor = Color.White;
+            roundedButton2.HoverColor = Color.Transparent;
+            roundedButton2.Location = new Point(169, 508);
+            roundedButton2.Name = "roundedButton2";
+            roundedButton2.Size = new Size(216, 53);
+            roundedButton2.TabIndex = 23;
+            roundedButton2.Text = "Dodaj";
+            roundedButton2.TextColor = Color.White;
+            roundedButton2.TooltipText = null;
+            roundedButton2.UseVisualStyleBackColor = false;
+            roundedButton2.Click += roundedButton2_Click;
+            // 
+            // dodaj_egzamin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(563, 597);
+            Controls.Add(roundedButton2);
+            Controls.Add(roundedButton1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox2);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Name = "dodaj_egzamin";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ContextMenuStrip contextMenuStrip1;
+        private ComboBox comboBox1;
+        private Label label1;
+        private ComboBox comboBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private RoundedButton roundedButton1;
+        private RoundedButton roundedButton2;
+    }
+}
